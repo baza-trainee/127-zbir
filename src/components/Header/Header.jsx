b20ef61c76f209ee1ed37ca2b9a7a5134adeb52f
bohdanalu <b_dana@ukr.net>
2024-06-11 23:31:44 +0300

    feat: add burger menu with animation and smooth scrolling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import logo from "../../assets/img/logo.svg";
+import burger from "../../assets/img/menu.svg";
 import S from "./header.module.scss";
 
 const menuItems = [
@@ -8,20 +10,43 @@ const menuItems = [
 ];
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleMenuItemClick = (e, href) => {
+    e.preventDefault();
+    const targetElement = document.querySelector(href);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: "smooth" });
+      setIsMenuOpen(false);
+    }
+  };
+
   return (
     <header className={S.header}>
       <div className="container">
         <nav className={S.header_nav}>
-          <a href="">
-            <img src={logo} alt="Logo" />
+          <a href="#">
+            <img className={S.header_logo} src={logo} alt="Logo" />
           </a>
-          <ul className={S.header_menu}>
+          <ul className={`${S.header_menu} ${isMenuOpen ? S.open : ""}`}>
             {menuItems.map((item, index) => (
               <li key={index} className={S.header_item}>
-                <a href={item.href}>{item.name}</a>
+                <a
+                  href={item.href}
+                  onClick={(e) => handleMenuItemClick(e, item.href)}
+                >
+                  {item.name}
+                </a>
               </li>
             ))}
           </ul>
+          <div className={S.header_burger} onClick={toggleMenu}>
+            <img src={burger} alt="burger-menu" />
+          </div>
         </nav>
       </div>
     </header>
