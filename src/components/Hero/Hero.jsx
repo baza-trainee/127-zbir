ad6eccde9529025043cb316954b2b1ca1ec30427
bohdanalu <b_dana@ukr.net>
2024-06-12 14:00:52 +0300

    feat: add basic structure to hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,17 @@
 import S from "./hero.module.scss";
 const Hero = () => {
   return (
-    <section className="section">
+    <section className={S.hero}>
       <div className="container">
-        <h1 className={S.hero_title}>
-          Збір на дрони для 127-мої бригади ТРО ЗСУ
-        </h1>
+        <div className={S.hero_content}>
+          <h1 className={S.hero_title}>
+            Збір на дрони для 127-мої бригади ТРО ЗСУ
+          </h1>
+          <div className={S.hero_btns}>
+            <a href="#">Підтримати</a>
+            <a href="">Поділитися</a>
+          </div>
+        </div>
       </div>
     </section>
   );
