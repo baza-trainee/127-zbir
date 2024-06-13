197f11c349fb5248982ac968bf940526441a34b0
vitalyiy-pritula <pritula.vitaly94@gmail.com>
2024-06-13 16:27:02 +0300

    підключив поп уп для блоку "Навіщо нам потрібні дрони?"

--- src/components/Reason/Reason.jsx
+++ src/components/Reason/Reason.jsx
@@ -1,3 +1,4 @@
+import SupportShareButtons from "../SupportShareButtons/SupportShareButtons";
 import S from "./reason.module.scss";
 
 const Reason = () => {
@@ -7,19 +8,19 @@ const Reason = () => {
         <h2 className="title">Навіщо нам потрібні дрони?</h2>
         <div className={S.reason__items}>
           <div className={S.reason__item}>
-            <h3 className={S.reason__title}>Розвідка і спостереження</h3>
+            <h3 className={S.subtitle}>Розвідка і спостереження</h3>
             <p className={S.reason__text}>Дрони дозволяють ефективно виявляти позиції противника, здійснювати розвідку місцевості та своєчасно реагувати на загрози.</p>
           </div>
           <div className={S.reason__item}>
-            <h3 className={S.reason__title}>Забезпечення безпеки</h3>
+            <h3 className={S.subtitle}>Забезпечення безпеки</h3>
             <p className={S.reason__text}>Використання дронів значно знижує ризик для життя наших військових, адже вони можуть виконувати небезпечні завдання дистанційно.</p>
           </div>
           <div className={S.reason__item}>
-            <h3 className={S.reason__title}>Керування вогню</h3>
+            <h3 className={S.subtitle}>Керування вогню</h3>
             <p className={S.reason__text}>Дрони допомагають точно наводити артилерійський вогонь на цілі, що підвищує ефективність наших атак і зменшує витрати боєприпасів.</p>
           </div>
           <div className={S.reason__item}>
-            <h3 className={S.reason__title}>Моніторинг</h3>
+            <h3 className={S.subtitle}>Моніторинг</h3>
             <p className={S.reason__text}> З їхньою допомогою можна оперативно контролювати ситуацію на полі бою та швидко приймати рішення.</p>
           </div>
         </div>
@@ -27,9 +28,8 @@ const Reason = () => {
           <p>Ваш внесок може зробити різницю між життям і смертю для наших бійців. Кожна гривня наближає нас до перемоги і допомагає захистити нашу Батьківщину.</p>
           <p>Разом ми сильні! Підтримайте 127-у бригаду і допоможіть їм виконувати свої обов’язки з максимальною ефективністю.</p>
         </div>
-        <div className={S.reason__actions}>
-          <a href="" target="_blank" className={S.reason__link}>Підтримати</a>
-          <a href="" target="_blank" className={S.reason__link}>Поділитися</a>
+        <div className={S.details__btn_wrapper}>
+        <SupportShareButtons />
         </div>
       </div>
     </section>
---
+++ src/components/SupportShareButtons/SupportShareButtons.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import arrowSvg from "../../assets/img/arrow-up-right.svg";
+import S from "../SupportShareButtons/SupportShareButtons.module.scss";
+
+const SupportShareButtons = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div className={S.btns}>
+      <a
+        href="https://www.monobank.ua"
+        className="btn_primary"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Підтримати
+      </a>
+      <button className="btn_secondary" onClick={openModal}>
+        Поділитися
+        <img src={arrowSvg} alt="Arrow icon" className="arrow-icon" />
+      </button>
+
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <a href="https://www.facebook.com">Facebook</a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SupportShareButtons;
